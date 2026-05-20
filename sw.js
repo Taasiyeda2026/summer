b533c1981c22f68fc50494e2a11f8e22b0abe62c
Idan Nahum <idanahum1122@gmail.com>
2026-05-21 01:56:20 +0300

    Redesign escape room intro into 3-card layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "summer-catalog-v2026-05-20-text-wrap-fix-v1";
+const CACHE_NAME = "summer-catalog-v2026-05-20-escape-intro-cards-v1";
 const ASSETS = [
   "./",
   "./index.html",
