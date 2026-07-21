fae0fdc49ee45cea5ae8c4c7ab38c0a5ccf2caf9
Idan Nahum <idanahum1122@gmail.com>
2026-07-21 23:56:25 +0300

    Restore Supabase public key for partnership form

--- programs-center/support-form.js
+++ programs-center/support-form.js
@@ -5,7 +5,7 @@ const formSubject = document.getElementById('formSubject');
 const partnershipRoute = document.getElementById('partnershipRoute');
 
 const SUPABASE_URL = 'https://szinlhjuwyiyszdpsdop.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzenNpbmxoanV3eWl5c3pkcHNk b3AiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3Nzk3MjI5OCwiZXhwIjoyMDkzNTQ4Mjk4fQ.yOK5rkApbYd4jbLAA_FR3F9JvBXJU_6wWCiReu0k70Q'.replace(' ', '');
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aW5saGp1d3lpeXN6ZHBzZG9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NzIyOTgsImV4cCI6MjA5MzU0ODI5OH0.yOK5rkApbYd4jbLAA_FR3F9JvBXJU_6wWCiReu0k70Q';
 
 function setFormStatus(message, type = '') {
   if (!formStatus) return;
