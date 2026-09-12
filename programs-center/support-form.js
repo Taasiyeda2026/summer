c175a764d331346fe70e5ef5a30536e58a2a36fe
Idan Nahum <idanahum1122@gmail.com>
2026-09-13 02:38:21 +0300

    Temporarily disable CC for programs-center form test

--- programs-center/support-form.js
+++ programs-center/support-form.js
@@ -54,7 +54,7 @@ const formSubject = document.getElementById('formSubject');
 const partnershipRoute = document.getElementById('partnershipRoute');
 
 const SUPABASE_URL = 'https://szinlhjuwyiyszdpsdop.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aW5saGp1d3lpeXN6ZHBzZG9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NzIyOTgsImV4cCI6MjA5MzU0ODI5OH0.yOK5rkApbYd4jbLAA_FR3F9JvBXJU_6wWCiReu0k70Q';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYXNlIiwicmVmIjoic3ppbmxoanV3eWl5c3pkcHNk b3AiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3Nzk3MjI5OCwiZXhwIjoyMDkzNTQ4Mjk4fQ.yOK5rkApbYd4jbLAA_FR3F9JvBXJU_6wWCiReu0k70Q'.replace(' ', '');
 
 function setFormStatus(message, type = '') {
   if (!formStatus) return;
@@ -127,6 +127,9 @@ if (supportPartnershipForm) {
       console.error(error);
       // שליחת המייל נשארת פעילה גם אם השמירה במסד הנתונים אינה זמינה זמנית.
     }
+
+    const ccField = supportPartnershipForm.querySelector('input[name="_cc"]');
+    if (ccField) ccField.remove();
 
     HTMLFormElement.prototype.submit.call(supportPartnershipForm);
   });
